465c008bfd8ff6f10f114b834902bc0076d5dff5
Tadiwa <mimi.ngwerume@gmail.com>
2024-05-07 15:05:01 +0200

    vsCode addition class

---
+++ Ngwerume220700842.java
@@ -0,0 +1,13 @@
+
+public class Ngwerume220700842 {
+
+    public static void main(String[] args) {
+      // Replace these with your actual numbers
+      int digit_1 = 10;
+      int digit_2 = 20;
+  
+      int sum = digit_1 + digit_2;
+  
+      System.out.println("The sum of " + digit_1 + " and " + digit_2 + " is: " + sum);
+    }
+  }
